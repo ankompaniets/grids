76485fb6d66f5540d84328fd551455d7bb7a8292
Andrey Kompaniets <andrey@poravinternet.ru>
2019-01-28 10:56:51 +0300

    добавлен плавный скролл

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
+$(document).ready(function() {
 
+	//плавный переход по ссылке
+
+	$('a[href^="#"]').click(function () {
+
+		elementClick = $(this).attr("href");
+		destination = $(elementClick).offset().top;
+		
+		$('html').animate( { scrollTop: destination }, 1100 );
+		
+		return false;
+	});
+});
