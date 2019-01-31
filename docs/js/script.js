84a07e90d9ba545c6e600ca7efc313d1fa754343
Andrey Kompaniets <andrey@poravinternet.ru>
2019-01-31 14:32:16 +0300

    hard work

--- docs/js/script.js
+++ docs/js/script.js
@@ -11,4 +11,40 @@ $(document).ready(function() {
 		
 		return false;
 	});
+
+	//динамика меню на мобильном
+
+	$('.js-header__nav-toggle').click(function () {
+
+		$(this).toggleClass('header__nav-toggle--active');
+
+		$('.header__nav').toggle('fast');
+	});
+
+	$(document).mouseup(function (e){
+
+		var menu = $('.header__nav'),
+			menuBtn = $('.header__nav-toggle');
+
+		if (!menuBtn.is(e.target) &&
+			menuBtn.has(e.target).length === 0 && 
+			!menu.is(e.target) && 
+			menu.has(e.target).length === 0) {
+
+			menu.hide('fast');
+			menuBtn.removeClass('header__nav-toggle--active');
+		}
+	});
+
+	//появление блока header__nav при ресайзе и возвращение header__nav-toggle в неактивное состояние
+
+	$( window ).resize(function() {
+	  if (window.matchMedia('(min-width: 769px)').matches) {
+	  	$('.header__nav').css('display', 'grid');
+	  	$('.js-header__nav-toggle').removeClass('header__nav-toggle--active');
+	  } 
+	  else {
+	  	$('.header__nav').removeAttr('style');
+	  }
+	});
 });
